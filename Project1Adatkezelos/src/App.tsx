d5fcc79004838ce9e14d3d81413c0bd069200b3f
Saso <msaso0725@gmail.com>
2025-10-09 09:43:02 +0200

    Redo

--- Project1Adatkezelos/src/App.tsx
+++ Project1Adatkezelos/src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <>
-      
+        <div>
+          <h1>Adatkezelő napló</h1>
+          <button>Bejelentkezés diákként</button>
+          <button>Bejelentkezés tanárként</button>
+        </div>
     </>
   )
 }
