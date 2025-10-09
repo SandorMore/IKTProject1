96334898a55989ffcf611e5ffee28a7f4e34ee07
Saso <msaso0725@gmail.com>
2025-10-09 09:47:22 +0200

    Done

--- Project1Adatkezelos/src/App.tsx
+++ Project1Adatkezelos/src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from 'react'
 import './App.css'
 
@@ -13,10 +12,5 @@ function App() {
         </div>
     </>
   )
-=======
-function App() {
-  return <div className="m-auto text-center text-3xl">Hello World!</div>;
->>>>>>> 52630675af5460b6057383db462ba45d37f7f3bb
 }
-
 export default App;
