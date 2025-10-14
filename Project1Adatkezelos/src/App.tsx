88e7d1597ebe490a61c039b9bde32c22962608de
Your Name <you@example.com>
2025-10-14 10:23:35 +0200

    deleted some files

--- Project1Adatkezelos/src/App.tsx
+++ Project1Adatkezelos/src/App.tsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
- 
   return (
-    <>
-        <div>
-          <h1>Adatkezelő napló</h1>
-          <button>Bejelentkezés diákként</button>
-          <button>Bejelentkezés tanárként</button>
-        </div>
-    </>
-  )
+    <div className="w-1/3 h-1/2 text-center border-[3px] rounded-sm p-5 justify-self-center self-center">
+      <h1 className="">Adatkezelő napló</h1>
+      <div className="flex flex-col items-center gap-5 mt-10">
+        <button className="border-amber-400 border-[2px] px-3 py-1 w-1/2 rounded-sm hover:bg-amber-400 hover:text-white">
+          Bejelentkezés diákként
+        </button>
+        <button className="border-amber-400 border-[2px] px-3 py-1 w-1/2 rounded-sm hover:bg-amber-400 hover:text-white">
+          Bejelentkezés tanárként
+        </button>
+      </div>
+    </div>
+  );
 }
 export default App;
